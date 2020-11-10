1a5801f62be333a0a775d9d63f53036bc2ee7eae
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-11-10 19:32:33 +0100

    Create models, put then in settings, add the path for images and migrate

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    #puedo buscar en la pagina cuales son los atributos que le puedo dar a la parte
+    #upload_to ----  django model fields
+    image = models.ImageField(upload_to='images/') #this makes that all images created, will be storage inside MEDIA folder, images
+
+    summary = models.CharField(max_length=200)
