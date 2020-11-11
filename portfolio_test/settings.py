e936833d72a9f0947268b75d16d1631a94204bdd
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-11-11 11:59:32 +0100

    Se creo un Admin, se permitio el acceso a los Jobs pero da error al intentar guardar la imagen

--- portfolio_test/settings.py
+++ portfolio_test/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#Para colocar otro sitio para guardar las imagenes simplemente '/directorio'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
