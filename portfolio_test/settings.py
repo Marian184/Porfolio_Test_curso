43a25b3a6376597992f41dbdfa69608064714d20
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-11-13 09:38:15 +0100

    add new Postgres Data Base

--- portfolio_test/settings.py
+++ portfolio_test/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'portfolio_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql',
+            #'django.db.backends.portfoliodb', #'django.db.backends.sqlite3',
+        'NAME': 'portfoliodb',
+            #'porfoliodb',# os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost', # lo mismo que 127.0.0.1
+        'PORT': '5432',
+
+
     }
 }
 
