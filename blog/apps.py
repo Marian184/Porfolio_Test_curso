804d005de1161aecc236954e3cb7be7b4a120422
Marian184 <m.fragueiro.lazcano@gmail.com>
2020-11-17 19:32:49 +0100

    Añadimos como ver imagen por imagen

--- blog/apps.py
+++ blog/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
-    name = 'blog'
+    name = 'blog'  #este nombre es el que va a tomar cuando llame a alguna propiedad blog.tuvieja blog.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 # Create your views here.
 def allblogs(request):
     blog = Blog.objects.all
     return render(request, 'blog/allblogs.html',{'blogs' :blog})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id) #pk es primary key
+    return render(request, 'blog/detail.html', {'blog':detail_blog})
